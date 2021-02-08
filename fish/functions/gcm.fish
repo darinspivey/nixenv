c96bd52539cbd32850d7dec5c1966688b31eaa19
Darin Spivey <darin.spivey@gmail.com>
2021-02-08 10:03:50 -0500

    fish: Update gcm.fish to work with main or master

--- fish/functions/gcm.fish
+++ fish/functions/gcm.fish
@@ -1,5 +1,14 @@
 function gcm --description 'Git checkout master'
-  git checkout master
-  git pull origin master -r
+  if git ls-remote --exit-code --heads origin refs/heads/master > /dev/null
+    set branch 'master'
+  else if git ls-remote --exit-code --heads origin refs/heads/main > /dev/null
+    set branch 'main'
+  else
+    echo "Neither branch 'master' nor 'main' was found!"
+    return 1
+  end
+  
+  git checkout $branch
+  git pull origin $branch -r
   npmrefresh
 end
